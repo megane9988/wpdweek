c9e5e1d3a2702747bfef219a913a5ece27315fb6
megane9988 <info@m-g-n.me>
2016-01-21 19:37:07 +0900

    facebookのjsを導入

--- wp-content/themes/wpdweek/header.php
+++ wp-content/themes/wpdweek/header.php
@@ -22,6 +22,14 @@
 </head>
 
 <body <?php body_class(); ?>>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v2.5&appId=307899779247453";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 <div id="page" class="site">
 	<div class="site-inner">
 		<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'twentysixteen' ); ?></a>
